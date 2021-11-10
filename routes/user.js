d58bb0bd5c92a29ef5ed0faa6af140d62d3f2795
Kevin <kevin03.nguyen@gmail.com>
2021-11-09 21:56:32 -0500

    fix register route

--- routes/user.js
+++ routes/user.js
@@ -12,12 +12,15 @@ router.get("/register", (req, res) => {
 //register user
 router.post(
   "/register",
-  asyncWrapper(async (req, res) => {
+  asyncWrapper(async (req, res, next) => {
     try {
       const { email, username, password } = req.body;
       const user = new User({ email, username });
       console.log(user);
       const newUser = await User.register(user, password);
+      req.login(newUser, (err) => {
+        if (err) return next(err);
+      });
       req.flash("success", "Welcome!");
       res.redirect("/campgrounds");
     } catch (e) {
@@ -40,7 +43,15 @@ router.post(
   }),
   (req, res) => {
     req.flash("success", "Welcome back!");
-    res.redirect("/campgrounds");
+    const redirectUrl = req.session.returnTo || "/campgrounds";
+    delete req.session.returnTo;
+    res.redirect(`${redirectUrl}`);
   }
 );
+//logout
+router.get("/logout", (req, res) => {
+  req.logout();
+  req.flash("success", "Logged out");
+  res.redirect("/campgrounds");
+});
 module.exports = router;
